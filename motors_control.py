db3d693cf3653f9a600ab9751151b1e3aeb9bcdc
lisovcev <panamskysw@icloud.com>
2024-05-14 10:26:14 +0300

    Add a new function of smooth speed exchange

--- motors_control.py
+++ motors_control.py
@@ -1,11 +1,21 @@
-def set_motor_power(power):
-    print(f"Установка мощности мотора на {power}%")
+import time
+def set_motor_speed(speed):
+    print(f"Установка скорости мотора на {speed}%")
+
+
+def smooth_set_speed(target_speed):
+    current_speed = 0
+    step = 1 if target_speed > current_speed else -1
+    for speed in range(current_speed, target_speed, step):
+        set_motor_speed(speed)
+        time.sleep(0.1)
+    set_motor_speed(target_speed)
 
 
 def main():
     while True:
-        power = input("Введите мощность мотора (0-100%): ")
-        set_motor_power(power)
+        speed = int(input("Введите скорость мотора (0-100%): "))
+        smooth_set_speed(speed)
 
 
 if __name__ == "__main__":
